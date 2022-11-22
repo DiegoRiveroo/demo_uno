6c6d0ea91d45063e855756728f9e44b9182c96ef
Diego <godie478@gmail.com>
2022-11-22 19:42:19 -0300

    git remote add origin https://github.com/DiegoRiveroo/demo_uno.git

---
+++ src/test/java/com/example/demo_uno/DemoUnoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_uno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoUnoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
